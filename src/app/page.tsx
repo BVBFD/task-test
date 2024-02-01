b0b94ee8a2f4f4f75bd56b237ce9ae3225a8a37e
BVBFD <lsevina126@gmail.com>
2024-02-01 14:13:07 +0900

    Feat(pagination): code pagination with nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,86 +1,125 @@
 "use client";
 
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
-import Image from "next/image";
 
-const fetchData = [
-  {
-    _id: 1,
-    imgUrl:
-      "https://res.cloudinary.com/dewa3t2gi/image/upload/v1704970199/myportfolioblogproject/s2bqbwyf1vml2beeqdrm.gif",
-  },
-  {
-    _id: 2,
-    imgUrl:
-      "https://res.cloudinary.com/dewa3t2gi/image/upload/v1677818589/myportfolioblogproject/ta2ew0kg9unlstsv3g3m.gif",
-  },
-  {
-    _id: 3,
-    imgUrl:
-      "https://res.cloudinary.com/dewa3t2gi/image/upload/v1679554666/myportfolioblogproject/qae9fi8bjtpuoc5qjse7.gif",
-  },
-  {
-    _id: 4,
-    imgUrl:
-      "https://res.cloudinary.com/dewa3t2gi/image/upload/v1678437768/myportfolioblogproject/qpyzc9k1w06m1ldplwmw.gif",
-  },
-  {
-    _id: 5,
-    imgUrl:
-      "https://res.cloudinary.com/dewa3t2gi/image/upload/v1676974927/myportfolioblogproject/od9rqmzhmhdmwusqk5qe.gif",
-  },
-];
-
-interface DataType {
-  _id: number;
-  imgUrl: string;
+interface PostType {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 }
 
 export default function Home() {
-  const [data, setData] = useState<Array<DataType>>([]);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const getData = async (fetchingData: Array<DataType>) => {
-    // HTTP 가상 Network Get Method 로직
-    await new Promise<DataType[]>((resolve, reject) =>
-      setTimeout(() => {
-        resolve(fetchingData);
-        reject("Fetch Data Failed!!");
-      }, 1000)
-    )
-      .then((result) => setData([...data, ...result]))
-      .catch((error) => console.log(error));
-    // HTTP 가상 Network Get Method 로직
+  const [totalDatas, setTotalDatas] = useState<PostType[]>([]);
+  const [totalPages, setTotalPages] = useState<number>();
+
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [showPageNumber, setShowPageNumber] = useState(5);
+
+  const [currentPageNumber, setCurrentPageNumber] = useState(1);
+  const [showDataRowsHTML, setShowDataRowsHTML] = useState<JSX.Element[]>();
+
+  const goToPage = (num: number) => {
+    const startRowIndex = (num - 1) * rowsPerPage;
+    const lastRowIndex = startRowIndex + rowsPerPage;
+    const showDataRows = totalDatas.slice(startRowIndex, lastRowIndex);
+
+    const htmlArr = showDataRows.map((row) => (
+      <div className={styles.dataRow}>{row.title}</div>
+    ));
+
+    setShowDataRowsHTML(htmlArr);
+    setCurrentPageNumber(num);
+  };
+
+  const renderPagination = () => {
+    const paginationNumBox = [];
+
+    paginationNumBox.push(
+      <button
+        onClick={() => goToPage(1)}
+        key={"<<"}
+        disabled={currentPageNumber === 1}
+      >
+        {"<<"}
+      </button>
+    );
+
+    paginationNumBox.push(
+      <button
+        onClick={() => goToPage(currentPageNumber - 1)}
+        key={"<"}
+        disabled={currentPageNumber === 1}
+      >
+        {"<"}
+      </button>
+    );
+
+    const startRangeNum =
+      Math.floor((currentPageNumber - 1) / showPageNumber) * showPageNumber + 1;
+    const endRangeNum = Math.min(
+      startRangeNum + (showPageNumber - 1),
+      totalPages as number
+    );
+
+    for (let i = startRangeNum; i <= endRangeNum; i += 1) {
+      paginationNumBox.push(
+        <button
+          key={i}
+          onClick={() => goToPage(i)}
+          disabled={i === currentPageNumber}
+          className={i === currentPageNumber ? `${styles.clicked}` : ""}
+        >
+          {i}
+        </button>
+      );
+    }
+
+    paginationNumBox.push(
+      <button
+        onClick={() => goToPage(currentPageNumber + 1)}
+        key={">"}
+        disabled={currentPageNumber === totalPages}
+      >
+        {">"}
+      </button>
+    );
+
+    paginationNumBox.push(
+      <button
+        onClick={() => goToPage(totalPages as number)}
+        key={">>"}
+        disabled={currentPageNumber === totalPages}
+      >
+        {">>"}
+      </button>
+    );
+
+    return paginationNumBox;
   };
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (containerRef.current) {
-        const { scrollHeight, clientHeight, scrollTop } = containerRef.current;
-
-        if (scrollTop + clientHeight >= scrollHeight) {
-          getData(fetchData);
-        }
-      }
+    const getTotalDatas = async () => {
+      const res = await fetch("/data/posts.json");
+      const totalDatas = await res.json();
+      setTotalDatas(totalDatas);
+      setTotalPages(Math.ceil(totalDatas.length / rowsPerPage));
     };
-    document.addEventListener("scroll", handleScroll);
 
-    return () => {
-      document.removeEventListener("scroll", handleScroll);
-    };
-  }, [data]);
-
-  useEffect(() => {
-    getData(fetchData);
+    getTotalDatas();
   }, []);
 
+  useEffect(() => {
+    goToPage(1);
+  }, [totalPages]);
+
   return (
-    <main className={styles.container} ref={containerRef}>
-      {data?.map((dt) => (
-        <div className={styles.imgBox}>
-          <Image src={`${dt.imgUrl}`} alt={`${dt._id}`} fill />
-        </div>
-      ))}
-    </main>
+    <>
+      <section className={styles.container}>
+        <main>{showDataRowsHTML}</main>
+        <footer>{totalPages && renderPagination()}</footer>
+      </section>
+    </>
   );
 }
